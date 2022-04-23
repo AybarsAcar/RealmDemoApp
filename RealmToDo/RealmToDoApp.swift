372a91e7dfb520a0a36e405ae82c9712e5001a49
Aybars Acar <aybars.dev@gmail.com>
2022-04-23 20:47:47 +1000

    Project Setup

---
+++ RealmToDo/RealmToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDoApp.swift
+//  RealmToDo
+//
+//  Created by Aybars Acar on 23/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
